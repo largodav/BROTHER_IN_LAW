f2019d4d82767c6780f1d0ee2d5555192cfa7828
David Garcia <albarranco2@gmail.com>
2025-01-01 16:31:16 +0100

    Tenemos el Div de cookies

--- CODIGO/ver0.1/scrap.py
+++ CODIGO/ver0.1/scrap.py
@@ -8,25 +8,19 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 
 driver.get('https://www.elcorteingles.es/search-nwx/')
+#Esperamos a que aparezca el div d elas cookies
+div_cookies = WebDriverWait(driver,10).until(EC.presence_of_all_elements_located((By.ID,"onetrust-accept-btn-handler")))
+div_cookies[0].click()
+#Buscamos el input de buscar
+boton_buscar = driver.find_element(by=By.CLASS_NAME, value="search-bar__input")
+boton_buscar.click()
 
-# boton_buscar = WebDriverWait(driver,10).until(
-#     EC.presence_of_all_elements_located((By.XPATH,"//input[@class='search-bar__input']"))    
-# )
-text = "mi texto"
-# boton_buscar[0].send_keys(text)
 
-#Entramos en la pagina de buscar y obtenemos la "barra de busqueda"
-element = driver.find_element(By.CLASS_NAME,"search-bar__input")
+boton_buscar.send_keys("hola")
 
-#Se detecta que salta una pantalla de "Politica de Cookies".... <div id="onetrust-group-container" class="ot-sdk-twelve ot-sdk-columns">...</div>
-#Que detexta un primer uso de la pagina. 
-#Debemos "saltarnos dicha pantalla"
-#El BOTON DE "ACEPTAR COOKIES" ES id="ontrust-accept-btn-handler".... DENTRO DE UN DIV GENERAL.. id="onetrust-banner-sdk"
 
-element.click()
-element.send_keys(text)
 
-driver.quit()
+
 
 
 
